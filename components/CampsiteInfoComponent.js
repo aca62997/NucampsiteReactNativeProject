e4f69021d0aac708157a3abf1297dee33387d60c
Amanda Abrams <aca62997@gmail.com>
2022-02-05 11:28:51 -0500

    Workshop Week 3 Task 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,7 +26,9 @@ function RenderCampsite(props) {
 
     const view = React.createRef();
 
-    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+  
+    const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -37,25 +39,29 @@ function RenderCampsite(props) {
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
-            }
-            return true;
+              Alert.alert(
+                  'Add Favorite',
+                  'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                  [
+                      {
+                          text: 'Cancel',
+                          style: 'cancel',
+                          onPress: () => console.log('Cancel Pressed')
+                      },
+                      {
+                          text: 'OK',
+                          onPress: () => props.favorite ?
+                              console.log('Already set as a favorite') : props.markFavorite()
+                      }
+                  ],
+                  { cancelable: false }
+              );
+              
+          } else if (recognizeComment(gestureState)) {
+            props.onShowModal();
+          }
+          
+          return true;
         }
     });
 
